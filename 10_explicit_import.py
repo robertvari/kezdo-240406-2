c43ceadba64532adfe88f8bb7db5764fbafa2e01
Robert Vari <mail.robertvari@gmail.com>
2024-04-20 16:33:48 +0200

    update

---
+++ 10_explicit_import.py
@@ -0,0 +1,9 @@
+from my_functions.math_functions import (
+    add_numbers, 
+    divide_numbers
+)
+
+add_result = add_numbers(10, 34)
+divide_result = divide_numbers(add_result, 5)
+
+print(divide_result)
---
+++ 11_import_all.py
@@ -0,0 +1,5 @@
+from my_functions.math_functions import *
+
+add_numbers(10, 20)
+multiply_numbers(10, 20)
+divide_numbers(10, 20)
