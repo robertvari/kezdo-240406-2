6798c3ebc8a6691d4c085c1cec9151dbe133bc93
Robert Vari <mail.robertvari@gmail.com>
2024-04-20 15:03:05 +0200

    Update

---
+++ 05_param_annotation.py
@@ -0,0 +1,7 @@
+def say_hello(name: str, address: str, age: int):
+    print(f"Hello {name}!")
+    print(f"You live in {address}.")
+    print(f"You are {age} years old.")
+
+
+say_hello("Robert", "Budapest", 32)
