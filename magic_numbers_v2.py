7e02d122a8ae907e6caa08838518f76cae20ed42
Robert Vari <mail.robertvari@gmail.com>
2024-04-20 17:47:01 +0200

    update

--- magic_numbers_v2.py
+++ magic_numbers_v2.py
@@ -39,6 +39,49 @@ def get_player_name():
 def ask_for_start_game(question):
     return input(f"{question} (y/n)")
 
+def game_loop():
+    global CREDITS
+
+    try_counter = 3
+
+    # Generate a random number
+    magic_number = random.randint(1, MAX_NUMBER)
+
+    # Get player number
+    player_number = input("Guess my number:")
+    
+    while str(magic_number) != player_number:
+        try_counter -= 1
+
+        if try_counter == 0:
+            break
+
+        print(f"Wrong guess! You have {try_counter} tries left. Try again.")
+        player_number = input("Guess my number:")
+
+    # End game conditions
+    if str(magic_number) == player_number:
+        print(f"You win! {magic_number} was my number :)")
+        CREDITS += 10
+    else:
+        print(f"You lost the game :( My number was {magic_number}")
+        CREDITS -= 10
+
+        if CREDITS <= 0:
+            print(f"I'm sorry {PLAYER_NAME}. You lost all your credits. Game Over:(")
+            exit()
+    
+    print(f"Now you have {CREDITS} credits")
+    player_input = ask_for_start_game("Do you want to play agai?")
+
+    if player_input != "y":
+        print("Have a nice day.")
+        exit()
+    
+    clear_screen()
+    game_loop()
+
+
 
 # Call main() and start program
 main()
