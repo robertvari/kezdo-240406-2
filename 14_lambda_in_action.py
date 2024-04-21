0312db6b941302ab179da243858c5701a56aa55f
Robert Vari <mail.robertvari@gmail.com>
2024-04-21 14:41:47 +0200

    update

--- 14_lambda_in_action.py
+++ 14_lambda_in_action.py
@@ -1,10 +1,17 @@
-my_friends = ["Vári Róbert", "Kiss Balázs", "Kovács Elemér", "Tóth Krisztina"]
+my_friends = ["Vári Róbert", "Kiss Balázs", "Kovács Elemér", "Tóth Krisztina", "Kiss Csaba Géza"]
 
-movies = {
-    1: {"title": "Lift", "score": 63},
-    2: {"title": "Napoleon", "score": 65},
-    3: {"title": "The Marvels", "score": 64},
-    4: {"title": "Aquaman and the Lost Kingdom", "score": 65},
-    5: {"title": "Society of the Snow", "score": 80},
-    6: {"title": "Deep Fear", "score": 53}
-}
+def get_name_for_sorting(name):
+    return name.split()[-1]
+
+# function reference
+# get_name_for_sorting
+
+# function call
+# get_name_for_sorting()
+
+default_sorting = sorted(my_friends)
+modified_sorting = sorted(my_friends, key=get_name_for_sorting)
+modified_sorting_lambda = sorted(my_friends, key=lambda name : name.split()[-1])
+print(default_sorting)
+print(modified_sorting)
+print(modified_sorting_lambda)
---
+++ 15_lambda_in_action_2.py
@@ -0,0 +1,10 @@
+movies = {
+    1: {"title": "Lift", "score": 63},
+    2: {"title": "Napoleon", "score": 65},
+    3: {"title": "The Marvels", "score": 64},
+    4: {"title": "Aquaman and the Lost Kingdom", "score": 65},
+    5: {"title": "Society of the Snow", "score": 80},
+    6: {"title": "Deep Fear", "score": 53}
+}
+
+
