44e249927527317731f174428b78b848469da266
Robert Vari <mail.robertvari@gmail.com>
2024-04-21 16:57:23 +0200

    update

---
+++ 20_recursion_in_action.py
@@ -0,0 +1,5 @@
+from my_functions.file_utils import get_all_files
+
+folder_path = r"D:\Work\PythonSuli\kezdo-240406\alapok_2"
+my_files = []
+get_all_files(folder_path, my_files)
--- my_functions/file_utils.py
+++ my_functions/file_utils.py
@@ -1,3 +1,5 @@
+import os
+
 def get_all_files(root_folder: str, file_list: list, ext: str=None):
     """
     Find all files in folder and it's subfolders.
@@ -8,4 +10,10 @@ def get_all_files(root_folder: str, file_list: list, ext: str=None):
         ext (str) optional: Extension filter    
     """
     
+    # Check if root_folder is valid
+    assert os.path.isdir(root_folder), f"root_folder is not valid: {root_folder}"
+
+    # Check if file_list is a list
+    assert isinstance(file_list, list), f"file_list must be of type list and not {type(file_list)}"
+
     pass
