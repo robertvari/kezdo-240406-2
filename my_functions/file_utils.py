e802701c767d2defa94823ec0879c7c11b47323a
Robert Vari <mail.robertvari@gmail.com>
2024-04-21 16:45:43 +0200

    update

--- my_functions/file_utils.py
+++ my_functions/file_utils.py
@@ -1,2 +1,11 @@
-def get_all_files(root_folder, file_list, ext=None):
+def get_all_files(root_folder: str, file_list: list, ext: str=None):
+    """
+    Find all files in folder and it's subfolders.
+
+    Parameters:
+        root_folder (str): Start folder
+        file_list (list): Empty list where we collect the files
+        ext (str) optional: Extension filter    
+    """
+    
     pass
