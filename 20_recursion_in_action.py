337f0e9ad205646029a8829cd97b64630d424e5c
Robert Vari <mail.robertvari@gmail.com>
2024-04-21 17:43:37 +0200

    update

--- 20_recursion_in_action.py
+++ 20_recursion_in_action.py
@@ -3,5 +3,3 @@ from my_functions.file_utils import get_all_files
 folder_path = r"D:\Work\PythonSuli\kezdo-240406\alapok_2\test_folder"
 my_files = []
 get_all_files(folder_path, my_files, ext="jpg")
-
-pass
