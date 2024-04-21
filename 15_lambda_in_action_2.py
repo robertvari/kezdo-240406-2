4f70df55c09a41cddcf90740f58bba6129f93cd2
Robert Vari <mail.robertvari@gmail.com>
2024-04-21 14:58:16 +0200

    update

--- 15_lambda_in_action_2.py
+++ 15_lambda_in_action_2.py
@@ -1,17 +1,18 @@
 movies = {
-    1: {"title": "Lift", "score": 63},
-    2: {"title": "Napoleon", "score": 65},
-    3: {"title": "The Marvels", "score": 64},
-    4: {"title": "Aquaman and the Lost Kingdom", "score": 65},
-    5: {"title": "Society of the Snow", "score": 80},
-    6: {"title": "Deep Fear", "score": 53}
+    1: {"title": "Lift", "score": 32},
+    2: {"title": "Napoleon", "score": 78},
+    3: {"title": "The Marvels", "score": 14},
+    4: {"title": "Aquaman and the Lost Kingdom", "score": 95},
+    5: {"title": "Society of the Snow", "score": 67},
+    6: {"title": "Deep Fear", "score": 52}
 }
 
 
 def get_score(movie_id):
-    print(movie_id)
-    return movie_id
+    return movies[movie_id]["score"]
 
 
-movies_sorted = sorted(movies, key=get_score)
-print(movies_sorted)
+movies_sorted = sorted(movies, key=get_score, reverse=False)
+
+for movie_id in movies_sorted:
+    print(f"Title:{movies[movie_id]['title']} Score: {movies[movie_id]['score']}")
